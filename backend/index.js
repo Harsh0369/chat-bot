f2631cfa27fc5139c40bc5eb2515d2757b1028e5
Harsh <harshagarwal81528@gmail.com>
2025-02-18 15:29:59 +0530

    frontend setup

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import connectDB from './database/db.js';
 
 import userRoutes from './Routes/userRoutes.js';
@@ -11,6 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/chats", chatRoutes);
