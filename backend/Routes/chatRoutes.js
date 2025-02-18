fb22ce4c528439c29011045dc95cb55cdcd9e128
Harsh <harshagarwal81528@gmail.com>
2025-02-18 14:46:28 +0530

    chat functionalities

--- backend/Routes/chatRoutes.js
+++ backend/Routes/chatRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createChat,getAllChats } from "../controllers/chatController.js";
+import { createChat,getAllChats,addConversation } from "../controllers/chatController.js";
 import { isAuth } from "../middlewares/isAuth.js";
 
 const router = express.Router();
 
 router.post("/new", isAuth, createChat);
 router.get("/all", isAuth, getAllChats);
+router.post("/:id", isAuth, addConversation);
 
 export default router;
--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -1,35 +1,55 @@
 import { Chat } from "../models/Chat.js";
 
 export const createChat = async (req, res) => {
-    try {
-        const userId = req.user._id;
-        const chat = await Chat.create({
-            user: userId,
-        })
-        res.json(chat);
-        
-    }catch(err) {
-        console.log(err);
-        res.status(500).json({ message: "Server Error" });
-    }
-}
- 
+  try {
+    const userId = req.user._id;
+    const chat = await Chat.create({
+      user: userId,
+    });
+    res.json(chat);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
 export const getAllChats = async (req, res) => {
-    try {
-        const chats = await Chat.find({ user: req.user._id }).sort({ createdAt: -1 });
-        res.json(chats);
-    }catch(err) {
-        console.log(err);
-        res.status(500).json({ message: "Server Error" });
-    }
-}
+  try {
+    const chats = await Chat.find({ user: req.user._id }).sort({
+      createdAt: -1,
+    });
+    res.json(chats);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
 export const addConversation = async (req, res) => {
-    const chat = await Chat.findById(req.params.id);
+  const chat = await Chat.findById(req.params.id);
 
-    if (!chat)
+  if (!chat) {
+    return res.status(404).json({ message: "Chat not found" });
+  }
+
+  const conversation = await Conversation.create({
+    chat: chat._id,
+    question: req.body.question,
+    answer: req.body.answer,
+  });
+
+  const updatedChat = await Chat.findByIdAndUpdate(
+    req.params.id,
     {
-        return res.status(404).json({ message: "Chat not found" });
+      latestMesage: req.body.question,
+    },
+    {
+      new: true,
     }
-
- }
+    );
+    
+    res.json({
+        conversation,
+        updatedChat,
+    })
+};
