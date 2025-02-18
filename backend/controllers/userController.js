3a5406ee04d8bba6c108674b9210d9a18ffd6e33
Harsh <harshagarwal81528@gmail.com>
2025-02-18 13:09:13 +0530

    some changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,71 +1,78 @@
 import sendMail from "../middlewares/sendMail.js";
 import { User } from "../models/User.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export const loginUser = async (req, res) => {
-    const { email} = req.body;
-    try {
-        let user = await User.findOne({ email });
+  try {
+    const { email } = req.body;
 
-        if (!user)
-        {
-            user = await User.create({
-                email
-            })
-        }
+    let user = await User.findOne({ email });
 
-        const otp = Math.floor(Math.random() * 100000);
-        const verifyToken = jwt.sign({ email, otp }, process.env.JWT_SECRET, {
-            expiresIn: "5m"
-        })
-
-        await sendMail(email, "OTP FOR LOGIN", otp);
-        
-        res.json({
-            message: "otp sent to your mail",
-            verifyToken
-        })
+    if (!user) {
+      user = await User.create({
+        email,
+      });
     }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({ message: "Server Error" });
-    }
-}
+
+    const otp = Math.floor(Math.random() * 1000000);
+
+    const verifyToken = jwt.sign({ user, otp }, process.env.Activation_sec, {
+      expiresIn: "5m",
+    });
+
+    await sendMail(email, "ChatBot", otp);
+
+    res.json({
+      message: "Otp send to your mail",
+      verifyToken,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
 export const verifyUser = async (req, res) => {
-    try {
-        const { otp, verifyToken } = req.body;
-        const verify = jwt.verify(verifyToken, process.env.JWT_SECRET);
-        if(!verify) {
-            return res.status(400).json({ message: "Invalid Token" });
-        }
-        if (verify.otp !== otp) {
-            return res.status(400).json({ message: "User Verified" });
-        }
+  try {
+    const { otp, verifyToken } = req.body;
 
-        const token = jwt.sign({ _id: verify.user._id }, process.env.JWT_SECRET, {
-            expiresIn
-        })
+    const verify = jwt.verify(verifyToken, process.env.Activation_sec);
 
-        res.json({
-            message: "User Verified",
-            token
-        })
-    }
-    catch(err) {
-        console.log(err)
-        res.status(500).json({ message: "Server Error" });
-    }
- }
+    if (!verify)
+      return res.status(400).json({
+        message: "Otp Expired",
+      });
 
-export const myProfile = async (req, res) => { 
-    try {
-        const user = await User.findById(req.user._id);
+    if (verify.otp !== otp)
+      return res.status(400).json({
+        message: "Wrong otp",
+      });
 
-        res.json(user);
-    }
-    catch(err) {
-        console.log(err);
-        res.status(500).json({ message: "Server Error" });
-    }
-}
+    const token = jwt.sign({ _id: verify.user._id }, process.env.Jwt_sec, {
+      expiresIn: "5d",
+    });
+
+    res.json({
+      message: "Logged in successfully",
+      user: verify.user,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const myProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -8,7 +8,7 @@ export const isAuth = async (req, res, next) => {
     if (!token) {
       return res.status(401).json({ message: "No token provided" });
       }
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const decoded = jwt.verify(token, process.env.Jwt_sec);
       if(!decoded) {
           return res.status(401).json({ message: "Invalid Token" });
       }
