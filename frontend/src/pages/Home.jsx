5453ccc76fa53c7badb47da140ba20e88c97df02
Harsh <harshagarwal81528@gmail.com>
2025-02-19 16:15:11 +0530

    gemini api integration

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../components/Sidebar'
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <div>
-      <Sidebar/>
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar } />
     </div>
   )
 }
